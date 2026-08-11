f8eff5a05c17a7e2c36f975f6bbc7f7e6e76acca
kill-ux <mustaphaboutoubdev@gmail.com>
2026-08-11 18:14:13 +0100

    test ci cd

--- tests/integration/test_gateway_integration.py
+++ tests/integration/test_gateway_integration.py
@@ -1,7 +1,5 @@
 import requests
 import os
-import time
-import pytest
 
 GATEWAY_URL = os.getenv("GATEWAY_URL", "http://api-gateway:3000")
 
@@ -12,12 +10,28 @@ def test_gateway_forwards_get_request_to_inventory():
 
     assert response.status_code == 200
     body = response.json()
-    assert body["id"] == 1
+    assert body[0]["id"] == 1
 
-# def test_gateway_forwards_get_request_to_billing():
-#     """GET through gateway reaches billing-app and returns the item."""
-#     response = requests.get(f"{GATEWAY_URL}/api/movies/1")
+def test_gateway_forwards_get_request_to_billing():
+    """GET through gateway reaches billing-app and returns the item."""
+    response = requests.get(f"{GATEWAY_URL}/api/billing")
 
-#     assert response.status_code == 200
-#     body = response.json()
-#     assert body["id"] == 1
+    assert response.status_code == 200
+    body = response.json()
+    assert body[0]["id"] == 1
+    
+def test_gateway_forwards_post_request_to_billing():
+    """POST through gateway reaches billing-app and creates a new billing item."""
+    payload = {
+        "user_id": 103,
+        "number_of_items": 2,
+        "total_amount": 89.99
+    }
+
+    response = requests.post(f"{GATEWAY_URL}/api/billing", json=payload)
+
+    assert response.status_code == 202
+    
+    body = response.json()
+    assert body["message"] == "Order request accepted"
+    
